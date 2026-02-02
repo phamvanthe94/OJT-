d356427b9b9ace373efe8d84638f28d9775354ec
phamvanthe <phamvanthe.ldkt@gmail.com>
2026-02-02 21:54:32 +0900

    feat(home):implement festival detail

--- src/main/java/com/ra/base_spring_boot/services/homeService/IFestivalHomeService.java
+++ src/main/java/com/ra/base_spring_boot/services/homeService/IFestivalHomeService.java
@@ -1,5 +1,6 @@
 package com.ra.base_spring_boot.services.homeService;
 
+import com.ra.base_spring_boot.dto.resp.homeresp.FestivalDetailResponse;
 import com.ra.base_spring_boot.dto.resp.homeresp.FestivalListResponse;
 import org.springframework.data.domain.Page;
 
@@ -7,4 +8,5 @@ public interface IFestivalHomeService {
 
     Page<FestivalListResponse> getFestivals(int page, int size);
 
+    FestivalDetailResponse getFestivalDetail(Long id);
 }
