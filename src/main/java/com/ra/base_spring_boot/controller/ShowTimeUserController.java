cf7684e4031b3cf3863110d9dcaa7b1e63789453
Phu <nguyenphu16122002@gmail.com>
2026-02-03 22:11:46 +0900

    feat: user select showtime by movie

---
+++ src/main/java/com/ra/base_spring_boot/controller/ShowTimeUserController.java
@@ -0,0 +1,25 @@
+package com.ra.base_spring_boot.controller;
+
+import com.ra.base_spring_boot.services.IShowTimeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user/showtimes")
+@RequiredArgsConstructor
+public class ShowTimeUserController {
+        @Autowired
+        private final IShowTimeService showTimeService;
+
+        @GetMapping
+        public ResponseEntity<?> getShowTimes(@RequestParam Long movieId) {
+            return ResponseEntity.ok(
+                    showTimeService.getShowTimesByMovie(movieId)
+            );
+        }
+    }
